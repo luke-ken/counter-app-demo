345cef8e562337ce0e19cff914e86bd77a595070
tuanpham <tuan.pham.se@gmail.com>
2024-10-28 17:06:30 +0100

    feat(backend): implement the according API to handle counting via JSON-RPC over HTTP, also add "cors" dependency into package.json

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require('cors')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const counterController = require("./routes/counterHTTPController")
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//for sake of simplicity due to local development, enable default CORS, allowed all origins
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use("/api", counterController)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
