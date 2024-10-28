732aa8fd9a8fcc14f8cd6117368681ffa1d99538
tuanpham <tuan.pham.se@gmail.com>
2024-10-28 14:51:55 +0100

    feat(frontend): add skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css'
+import {Provider} from "react-redux";
+import {store} from "./stores/store.ts";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {CounterWithReduxOnly} from "./components/CounterWithReduxOnly.tsx";
+import {CounterWithHTTP} from "./components/CounterWithHTTP.tsx";
+import {CounterWithWebSocket} from "./components/CounterWithWebSocket.tsx";
 
 function App() {
 
     return (
-        <>
-
-        </>
+        <Provider store={store}>
+            <BrowserRouter>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Redux only</Link></li>
+                        <li><Link to="/with-http">With HTTP</Link></li>
+                        <li><Link to="/with-websocket">With Websocket</Link></li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<CounterWithReduxOnly/>} />
+                    <Route path="/with-http" element={<CounterWithHTTP/>} />
+                    <Route path="/with-websocket" element={<CounterWithWebSocket/>} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     )
 }
 
