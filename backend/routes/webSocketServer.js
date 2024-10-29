a6b1f3dec926dcd0ef22e72504bc7e2f930d1a3e
tuanpham <tuan.pham.se@gmail.com>
2024-10-29 22:10:03 +0100

    feat(backend): implement according WebSocket API to handle the counting with JSON-RPC

---
+++ backend/routes/webSocketServer.js
@@ -0,0 +1,24 @@
+const counterWebSocket = (port) => {
+    const WebSocketServer = require("ws").WebSocketServer
+    const wss = new WebSocketServer({port, path: "/ws/counter"})
+
+    wss.on("connection", (ws) => {
+        ws.on("message", (message) => {
+            const {id, method, params} = JSON.parse(message.toString())
+
+            let result
+
+            if (method === "increment") {
+                result = params.value + 1
+            } else if (method === "decrement") {
+                result = params.value - 1
+            } else if (method === "reset") {
+                result = 0
+            }
+
+            ws.send(JSON.stringify({id, result}))
+        })
+    })
+}
+
+module.exports = {counterWebSocket}
